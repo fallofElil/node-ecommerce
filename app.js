b529ccd9449f37ba7b87d96c4a2698743a8ee4dd
Ilya Kiyanov <ikiyanov@protonmail.com>
2020-03-04 01:54:02 +0300

    beginning adding validator

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+//const expressValidator = require('express-validator');
 require('dotenv').config();
 
 //routes
@@ -23,6 +24,7 @@ mongoose
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+//app.use(expressValidator());
 
 //routes middleware
 app.use('/api', userRoutes);
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { check, oneOf, validationResult } = require('express-validator');
 
 const { signup } = require('../controllers/user');
+//const { userSignupValidator } = require('../validator');
 
-router.post('/signup', signup);
+router.post('/signup', oneOf([
+        check('name', 'Необходимо указать имя').notEmpty(),
+        check('email', 'Необходимо указать email')
+            .matches(/.+\@.+\..+/)
+            .withMessage('Email должен содержать @')
+            .isLength({
+                min: 4
+            }),
+        check('password', 'Необходимо указать пароль').notEmpty(),
+        check('password')
+            .isLength({min: 6})
+            .withMessage('Пароль должен содержать минимум 6 символов')
+        ], (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).json({ errors: errors.array() });
+            }
+            next();
+        }
+), signup);
 
 module.exports = router;
