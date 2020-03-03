d20cf0159c83d50ad2001a33bbb9b30ba50aea26
Ilya Kiyanov <ikiyanov@protonmail.com>
2020-03-04 00:41:49 +0300

    adds helpers for displaying error message

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,13 @@
 const User = require('../models/user');
+const {errorHandler} = require('../helpers/dbErrorHandlers');
 
 exports.signup = (req, res) => {
-    console.log(`request user body: ${req.body}`);
+    console.log("request user body:", req.body);
     const user = new User(req.body);
     user.save((err, user) => {
         if(err) {
             return res.status(400).json({
-                err
+                err: errorHandler(err)
             });
         }
 
