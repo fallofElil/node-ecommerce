a4065279b11d3ad0ef6482f1851a85b218492dd9
Ilya Kiyanov <ikiyanov@protonmail.com>
2020-03-22 16:20:18 +0300

    adds correct validation of user sign up

--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,37 @@
 const express = require('express');
 const router = express.Router();
+
 const { check, oneOf, validationResult } = require('express-validator');
 
 const { signup } = require('../controllers/user');
 //const { userSignupValidator } = require('../validator');
 
-router.post('/signup', oneOf([
-        check('name', 'Необходимо указать имя').notEmpty(),
-        check('email', 'Необходимо указать email')
-            .matches(/.+\@.+\..+/)
-            .withMessage('Email должен содержать @')
-            .isLength({
-                min: 4
-            }),
-        check('password', 'Необходимо указать пароль').notEmpty(),
+router.post('/signup', [
+        check('name')
+            .not()
+            .isEmpty()
+            .withMessage('Поле имени не должно быть пустыим'),
+        check('email')
+            .isEmail()
+            .withMessage('Укажите корректное значение Email'),
+        check('password')
+            .not()
+            .isEmpty()
+            .withMessage('Поле пароля не должно быть пустым'),
         check('password')
             .isLength({min: 6})
             .withMessage('Пароль должен содержать минимум 6 символов')
         ], (req, res, next) => {
             const errors = validationResult(req);
+            console.log(req.body);
+
             if (!errors.isEmpty()) {
-                return res.status(422).json({ errors: errors.array() });
+                return res.status(422).json(errors.array());
+            } else {
+                res.send({});
             }
             next();
         }
-), signup);
+ , signup);
 
 module.exports = router;
